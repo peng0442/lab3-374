c82af9e30f7e74b6e534960a75555eb5aaa5e567
peng0442 <peng0442@gmail.com>
2016-10-27 00:58:21 -0600

    test

--- src/lab3/LinkedList.java
+++ src/lab3/LinkedList.java
@@ -103,13 +103,15 @@ public static void main(String[] args) {
 	list.addElement(b);
 	list.addElement(c);
 	list.addElement(d);
-//	list.printLinkedListHead();
-	
-//	list.getElement(2);
-	
-//	list.deleteElement(1);
-	
-//	list.printLinkedListHead();
+	System.out.println("Print list");
+	list.printLinkedListHead();
+	System.out.println("get element in index 2");
+	list.getElement(2);
+	System.out.println("delete element in index 1");
+	list.deleteElement(1);
+	System.out.println("Print list");
+	list.printLinkedListHead();
+	System.out.println("Print list from tail");
 	list.printLinkedListTail();
 }
 	
